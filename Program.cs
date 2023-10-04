c2b31f481090378b0108e04dccfb6ffea25a6674
Jovhor <dr.jovhor@mail.ru>
2023-10-04 22:04:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в создание двумерного массива!");
+            Console.WriteLine("Введите количество строк:");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов:");
+            int col = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение, до которого подбирать рандом:");
+            int randy = int.Parse(Console.ReadLine());
+
+            int[,] arrayA = new int[row, col];
+            int[,] arrayB = new int[row, col];
+
+            Random r = new Random();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arrayA[i, j] = r.Next(randy + 1);
+                    Console.Write($"{arrayA[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arrayB[i, j] = r.Next(randy + 1);
+                    Console.Write($"{arrayA[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write($"{arrayA[i, j] + arrayB[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
